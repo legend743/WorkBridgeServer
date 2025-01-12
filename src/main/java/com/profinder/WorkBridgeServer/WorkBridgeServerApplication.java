00350190e30ccabcbbfee3083ca8da80bf3d7ee2
Faisal <fmohd743@gmail.com>
2025-01-12 12:04:37 +0530

    authentication added

---
+++ src/main/java/com/profinder/WorkBridgeServer/WorkBridgeServerApplication.java
@@ -0,0 +1,13 @@
+package com.profinder.WorkBridgeServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkBridgeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkBridgeServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/profinder/WorkBridgeServer/WorkBridgeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.profinder.WorkBridgeServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkBridgeServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
